8ba0bc79f24c2c936914bd3f079d48b6daaf7d3e
Etienne <etienne.bardet@gmail.com>
2025-05-05 16:41:29 +0200

    fix: loss reduction led to Gaussianity of the latent variables to be overprioritized

--- loss.py
+++ loss.py
@@ -17,34 +17,50 @@ def cond_loss(
     recon_x, x, recon_y, y, mu1, logvar1, mu2, logvar2, mu3, logvar3, gamma, gamma2
 ):
     """
-    Conditional loss function for the VAE
-    recon_x: reconstructed HR image
-    x: original HR image
-    recon_y: reconstructed LR image
-    y: original LR image
-    mu1: mean of the latent variable for the LR image
-    logvar1: log variance of the latent variable for the LR image
-    mu2: mean of the latent variable for the HR image depending on the HR image
-    logvar2: log variance of the latent variable for the HR image depending on the HR image
-    mu3: mean of the latent variable for the HR image depending on the LR image
-    logvar3: log variance of the latent variable for the HR image depending on the LR image
-    gamma: variance of the prior
+    Loss function for the Conditional Super-Resolution VAE. This function minimizes the **NEGATIVE** ELBO: 
+    .. math::
+        -(E[log p(x|z)] - D_KL(q(z|x)||p(z))) .
 
-    Note: Parameters 1 and 2,3 represent respectively the latent variables of the u and z.
-    2 : z from x
-    3 : z from y,u
+    Args:
+        recon_x (torch.Tensor): Reconstructed high-resolution (HR) image.
+        x (torch.Tensor): Original high-resolution (HR) image.
+        recon_y (torch.Tensor): Reconstructed low-resolution (LR) image.
+        y (torch.Tensor): Original low-resolution (LR) image.
+        mu1 (torch.Tensor): Mean of the latent variable for the LR image.
+        logvar1 (torch.Tensor): Log variance of the latent variable for the LR image.
+        mu2 (torch.Tensor): Mean of the latent variable for the HR image conditioned on the HR image.
+        logvar2 (torch.Tensor): Log variance of the latent variable for the HR image conditioned on the HR image.
+        mu3 (torch.Tensor): Mean of the latent variable for the HR image conditioned on the LR image.
+        logvar3 (torch.Tensor): Log variance of the latent variable for the HR image conditioned on the LR image.
+        gamma (torch.Tensor): Variance of the prior for the HR image.
+        gamma2 (torch.Tensor): Variance of the prior for the LR image.
+
+    Returns:
+        tuple: A tuple containing:
+            - mse_x (torch.Tensor): Mean squared error for the HR image.
+            - kld_u (torch.Tensor): Kullback-Leibler divergence for the LR image.
+            - mse_y (torch.Tensor): Mean squared error for the LR image.
+            - kld_z (torch.Tensor): Kullback-Leibler divergence for the HR image.
+
+    Note:
+        Parameters 1 (mu1, logvar1) and 2,3 (mu2, logvar2, mu3, logvar3) represent the latent variables for `u` and `z`, respectively:
+        - 2: `z` derived from `x`.
+        - 3: `z` derived from `y` and `u`.
     """
     d = mu1.size(1)
-    mse_x = d * (
-        F.mse_loss(recon_x, x, reduction="mean") / (2 * gamma.pow(2)) + (gamma.log())
-    )
-    kld_u = 0.5 * torch.sum(mu1.pow(2) + logvar1.exp() - 1 - logvar1)
     mse_y = d * (
         F.mse_loss(recon_y, y, reduction="mean") / (2 * gamma2.pow(2)) + (gamma2.log())
     )
-    kld_z = 0.5 * (
-        torch.sum(logvar3 - logvar2 - 1)
-        + torch.sum((logvar2 - logvar3).exp())
-        + torch.sum((mu2 - mu3).pow(2) * ((-logvar3).exp()))
+    kld_u = 0.5 * torch.sum(mu1.pow(2) + logvar1.exp() - 1 - logvar1, dim=1).mean()
+    mse_x = d * (
+        F.mse_loss(recon_x, x, reduction="mean") / (2 * gamma.pow(2)) + (gamma.log())
+    )
+    kld_z = (
+        0.5
+        * (
+            torch.sum(logvar3 - logvar2 - 1, dim=1)
+            + torch.sum((logvar2 - logvar3).exp(), dim=1)
+            + torch.sum((mu2 - mu3).pow(2) * ((-logvar3).exp()), dim=1)
+        ).mean()
     )
     return mse_x, kld_u, mse_y, kld_z
