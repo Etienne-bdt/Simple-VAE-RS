2ca17705acc3ab8da84a3e5e8b6451f5593fa4bd
Etienne <etienne.bardet@gmail.com>
2025-06-03 11:40:22 +0200

    fix: loss coefficient I hadn't modified because I had stopped using this model

--- loss/vae_loss.py
+++ loss/vae_loss.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 def base_loss(recon_x, x, mu, logvar, gamma):
     # Define the loss function for the VAE
     # Gamma is the variance of the prior
-    d = mu.size(1)
+    d = recon_x.shape[0] * recon_x.shape[1] * recon_x.shape[2] * recon_x.shape[3]
     mse = d * (
         F.mse_loss(recon_x, x, reduction="mean") / (2 * gamma.pow(2)) + (gamma.log())
     )
