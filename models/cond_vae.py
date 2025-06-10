fix(maybe fix): modify some latent computation to make it match

models/cond_vae.py
@@ -18,8 +18,8 @@ class Cond_SRVAE(BaseVAE):
             callbacks = []
         super(Cond_SRVAE, self).__init__(patch_size, callbacks)
         self.cr = cr
-        self.latent_size = int(patch_size * patch_size * 4 // self.cr)
-        self.latent_size_y = int(self.latent_size // 4)
+        self.latent_size = int(patch_size * patch_size * 4 / self.cr)
+        self.latent_size_y = int(self.latent_size / 4)
         self.patch_size = patch_size
         self.gammax = torch.tensor(1.0, requires_grad=True)
         self.gammay = torch.tensor(1.0, requires_grad=True)
@@ -90,7 +90,7 @@ class Cond_SRVAE(BaseVAE):
 
         u = self.u_to_z(u)
         u = u.view(u.size(0), -1)
-
+        print(f"y shape: {y.shape}, u shape: {u.shape}")
         jointure = torch.cat((y, u), dim=1)
 
         mu_u_y = self.mu_u_y_to_z(jointure)

models/layers.py
@@ -32,7 +32,7 @@ class downsample_sequence(nn.Module):
             num_steps: number of downsampling steps (optional, will be inferred if not given)
         """
         super(downsample_sequence, self).__init__()
-        out_flattened_size = int(math.prod(in_shape) // (compression_ratio * 2)) * 2
+        out_flattened_size = int(math.prod(in_shape) / compression_ratio)
         if out_flattened_size % 2 != 0:
             out_flattened_size += 1
         assert out_flattened_size is not None, "Must specify out_flattened_size"